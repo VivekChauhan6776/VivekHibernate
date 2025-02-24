c6b2235e249230d32592533000b11902fbc12782
VivekChauhan6776 <vivek969800chauhan@gmail.com>
2025-02-24 21:39:39 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/Main.java
@@ -0,0 +1,25 @@
+package com.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.hibernate.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee vivek = new Employee(1,"vivek", "male", 7363); 
+		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(vivek);
+		tx.commit();
+		
+
+	}
+
+}
