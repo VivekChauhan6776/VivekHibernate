b41caf0e366a1eefe45c738bb42fa3f31b7e2c96
VivekChauhan6776 <vivek969800chauhan@gmail.com>
2025-03-07 21:33:53 +0530

    update

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -1,7 +1,12 @@
 package com.hibernate;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.persister.internal.StandardPersisterClassResolver;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -14,7 +19,12 @@ public class Main {
 		Employee vivek = new Employee("vansh", "male", 7363); 
 //		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 //		SessionFactory sf = cfg.buildSessionFactory();
-		SessionFactory sf= new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+//		SessionFactory sf= new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata mate = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sf= mate.buildSessionFactory();
+		
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
 		session.persist(vivek);
